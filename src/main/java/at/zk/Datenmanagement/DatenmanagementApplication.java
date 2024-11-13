01642809769a575a5a9b3625a3dacbd3983da842
czlabinger <czlabinger@student.tgm.ac.at>
2024-11-13 18:36:14 +0100

    Initial commit

---
+++ src/main/java/at/zk/Datenmanagement/DatenmanagementApplication.java
@@ -0,0 +1,14 @@
+package at.zk.Datenmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatenmanagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatenmanagementApplication.class, args);
+	}
+
+
+}
